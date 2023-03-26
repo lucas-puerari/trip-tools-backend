ffc6fff101720f8d22f03c6c2cb07491e5ae3e11
Lucas Puerari <lucas.puerari@unimib.it>
2023-03-26 20:11:22 +0200

    Get the default log level from env vars

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,5 +1,8 @@
 import pino from 'pino';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default pino({
-  level: process.env.PINO_LOG_LEVEL || 'debug',
+  level: process.env.LOG_LEVEL || 'info',
 });
